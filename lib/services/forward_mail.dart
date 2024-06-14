3f9ad910ae63467403331691910c1439cfc34246
medhaa09 <medhaagarwal603@gmail.com>
2024-06-14 23:35:53 +0530

    added reply and forward mail  backend code

---
+++ lib/services/forward_mail.dart
@@ -0,0 +1,47 @@
+import 'package:enough_mail/enough_mail.dart';
+import 'package:flutter/material.dart';
+
+class EmailForward {
+  static Future<void> forwardEmail({
+    required String username,
+    required String password,
+    required MimeMessage originalMessage,
+    required String forwardTo,
+    required String forwardBody,
+    required Function(String, Color) onResult,
+  }) async {
+    final client = SmtpClient('enough_mail', isLogEnabled: true);
+    try {
+      await client.connectToServer('mmtp.iitk.ac.in', 465, isSecure: true);
+      await client.ehlo();
+
+      await client.authenticate(username, password, AuthMechanism.plain);
+
+      final originalPlainText = originalMessage.decodeTextPlainPart() ?? '';
+      final originalHtmlText = originalMessage.decodeTextHtmlPart() ?? '';
+      final forwardText = '\n\nForwarded message:\n\n$originalPlainText';
+      final fullForwardBody = '$forwardBody\n$forwardText';
+
+      final builder = MessageBuilder.prepareMultipartAlternativeMessage(
+        plainText: fullForwardBody,
+        htmlText: "<p>$forwardBody</p><blockquote>$originalHtmlText</blockquote>",
+      )
+        ..from = [MailAddress(username, '$username@iitk.ac.in')]
+        ..to = [MailAddress(forwardTo, forwardTo)]
+        ..subject = 'Fwd: ${originalMessage.decodeSubject()}';
+
+      final mimeMessage = builder.buildMimeMessage();
+      final sendResponse = await client.sendMessage(mimeMessage);
+
+      if (sendResponse.isOkStatus) {
+        onResult('Email forwarded successfully', Colors.green);
+      } else {
+        onResult('Failed to forward email: Failed to establish connection with server', Colors.red);
+      }
+    } catch (e) {
+      onResult('Failed to forward email: $e', Colors.red);
+    } finally {
+      await client.quit();
+    }
+  }
+}
