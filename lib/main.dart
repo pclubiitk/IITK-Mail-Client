4d310e96a3800348a00bdd5a716b6fbe61e77562
Krish Jain <krishjain23@iitk.ac.in>
2024-08-05 15:39:25 +0530

    Reduntant

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:connectivity_plus/connectivity_plus.dart';
 import 'package:flutter/material.dart';
-import 'package:iitk_mail_client/pages/DesktopUI/email_list.dart';
-import 'package:iitk_mail_client/pages/TabletUI/email_list.dart';
+// import 'package:iitk_mail_client/pages/DesktopUI/email_list.dart';
+// import 'package:iitk_mail_client/pages/TabletUI/email_list.dart';
 import 'package:iitk_mail_client/route_provider.dart';
 import 'package:provider/provider.dart';
 import 'Dependency_Injection.dart';
